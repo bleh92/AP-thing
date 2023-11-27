0aee7f9f1577623e6216e1827afbce1394ddcbc3
bleh92 <110454113+bleh92@users.noreply.github.com>
2023-11-27 20:02:08 +0530

    Update tools-installation.sh

--- tools-installation.sh
+++ tools-installation.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Function to check and handle errors
-check_error() {
-    if [ $? -ne 0 ]; then
-        echo "Error: $1"
-        exit 1
-    fi
-}
-
-# Check if running as root
-if [ "$(whoami)" != "root" ]; then
-    echo "This script must be run as root."
-    exit 1
+# Check if the script is being sourced
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    echo "Script is being sourced."
 else
-    echo "Script is running as root."
+    echo "ERROR: Script must be sourced using 'source' command."
+    exit 1
 fi
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "ERROR: This script must be run as root."
+    exit 1
+fi
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Check if Go is installed
 if command -v go &> /dev/null; then
@@ -66,8 +65,22 @@ check_error "Failed to install dnsmorph"
 #Installing checkdmarc
 echo "Installing checkdmarc..."
 sudo apt-get install python3-pip -y 
-pip3 install -U checkdmarc
 pip3 install -U git+https://github.com/domainaware/checkdmarc.git
+
+#Installing dkim-query
+gem install dkim-query
+
+#Installing check_rep.py
+git clone https://github.com/dfirsec/check_rep.git
+pip install -r "SCRIPT_DIR/check_rep/requirements.txt" 
+
+ 
+# Verify if dkim-query is installed
+if command -v dkim-query &> /dev/null; then
+    echo "dkim-query has been successfully installed."
+else
+    echo "Failed to install dkim-query. Please check the installation process."
+fi
 
 # Verify if checkdmarc is installed
 if command -v checkdmarc &> /dev/null; then
